2b1bdd3e1a04a5d76c0fa8608afe9fac026819af
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-06-21 13:37:39 +0500

    Start point

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
+from app.api.routers import main_router
+from app.core.config import settings
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title=settings.app_title)
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "FastAPI"}
+app.include_router(main_router)
