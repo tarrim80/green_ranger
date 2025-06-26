921693e0b426f80283f2833b3f60c7e6c0e60e11
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-06-26 19:22:57 +0500

    Add users endpoints

---
+++ backend/app/api/v1/__init__.py
@@ -0,0 +1,2 @@
+from app.api.v1.user import router as user_router
+from app.api.v1.role import router as role_router
---
+++ backend/app/api/v1/user.py
@@ -0,0 +1,43 @@
+from typing import Sequence
+
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import get_async_session
+from app.core.user import auth_backend, fastapi_users
+from app.models import User
+from app.schemas.user import UserCreate, UserRead, UserShortRead, UserUpdate
+
+router = APIRouter()
+
+router.include_router(
+    router=fastapi_users.get_auth_router(backend=auth_backend),
+    prefix="/auth/jwt",
+    tags=["Авторизация"],
+)
+router.include_router(
+    router=fastapi_users.get_register_router(
+        user_schema=UserRead, user_create_schema=UserCreate
+    ),
+    prefix="/auth",
+    tags=["Авторизация"],
+)
+router.include_router(
+    router=fastapi_users.get_users_router(
+        user_schema=UserRead, user_update_schema=UserUpdate
+    ),
+    prefix="/users",
+    tags=["Пользователи"],
+)
+
+
+@router.get(
+    "/users", response_model=list[UserShortRead], tags=["Пользователи"]
+)
+async def list_users(
+    session: AsyncSession = Depends(get_async_session),
+) -> list[UserShortRead]:
+    result = await session.execute(select(User))
+    users_db = result.scalars().all()
+    return [UserShortRead.model_validate(obj=user_db) for user_db in users_db]
