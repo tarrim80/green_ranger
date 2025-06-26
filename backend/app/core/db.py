17e6d7177af5304a38544150e49bd92cc218562e
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-06-26 19:18:03 +0500

    Update file (sorting imports)

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,26 +1,9 @@
 from typing import AsyncGenerator
 
-from app.core.config import settings
-from app.serviсes.case_converter import camel_case_to_snake_case
-from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from sqlalchemy.orm import declarative_base, declared_attr
 
-
-class PreBase:
-    __abstract__ = True
-
-    metadata = MetaData(
-        naming_convention=settings.naming_convention,
-    )
-
-    @declared_attr.directive
-    def __tablename__(cls) -> str:
-        return f"{camel_case_to_snake_case(cls.__name__)}"  # type: ignore
-
-
-Base = declarative_base(cls=PreBase)
+from app.core.config import settings
 
 engine = create_async_engine(url=settings.database_url)
 
--- backend/app/core/user.py
+++ backend/app/core/user.py
@@ -1,9 +1,5 @@
 from typing import Union
 
-from app.core.config import settings
-from app.core.db import get_async_session
-from app.models.user import User
-from app.schemas.user import UserCreate
 from fastapi import Depends, Request
 from fastapi_users import (
     BaseUserManager,
@@ -18,6 +14,11 @@ from fastapi_users.authentication import (
 )
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.config import settings
+from app.core.db import get_async_session
+from app.models.user import User
+from app.schemas.user import UserCreate
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
