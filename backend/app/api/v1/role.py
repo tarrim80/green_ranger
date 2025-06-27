eaa06e6aea4c9da8e340c4e572ec8880e0f06755
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-06-27 11:51:09 +0500

    Create role's endpoints

--- backend/app/api/v1/role.py
+++ backend/app/api/v1/role.py
@@ -8,7 +8,12 @@ from app.schemas import RoleCreate, RoleRead, RoleUpdate
 router = APIRouter()
 
 
-@router.get(path="/", response_model=list[RoleRead])
+@router.get(
+    path="/",
+    response_model=list[RoleRead],
+    summary="Получение списка ролей",
+    description="Показывает список всех возможных ролей пользователя.",
+)
 async def get_all_roles(
     repo: RoleRepository = Depends(),
 ) -> list[RoleRead]:
@@ -16,7 +21,12 @@ async def get_all_roles(
     return [RoleRead.model_validate(obj=role) for role in db_roles]
 
 
-@router.get(path="/{role_id}", response_model=RoleRead)
+@router.get(
+    path="/{role_id}",
+    response_model=RoleRead,
+    summary="Получение роли",
+    description="Показывает роль по ее идентификатору (id).",
+)
 async def get_role(role_id: int, repo: RoleRepository = Depends()) -> RoleRead:
     role_db = await repo.get(id=role_id)
     if not role_db:
@@ -28,7 +38,12 @@ async def get_role(role_id: int, repo: RoleRepository = Depends()) -> RoleRead:
 
 
 @router.post(
-    path="/", response_model=RoleRead, status_code=status.HTTP_201_CREATED
+    path="/",
+    response_model=RoleRead,
+    status_code=status.HTTP_201_CREATED,
+    summary="Создание новой роли",
+    description="Создает новую уникальную роль в системе. \
+        Используется для разграничения прав доступа пользователей.",
 )
 async def create_role(
     role_in: RoleCreate, repo: RoleRepository = Depends()
@@ -37,7 +52,13 @@ async def create_role(
     return RoleRead.model_validate(obj=role_db)
 
 
-@router.patch(path="/{role_id}", response_model=RoleRead)
+@router.patch(
+    path="/{role_id}",
+    response_model=RoleRead,
+    summary="Изменение роли",
+    description="Изменяет поля записи в конкретной роли \
+        по ее идентификатору (id).",
+)
 async def role_update(
     role_id: int, role_in: RoleUpdate, repo: RoleRepository = Depends()
 ) -> RoleRead:
@@ -51,8 +72,13 @@ async def role_update(
     return RoleRead.model_validate(obj=role_update_db)
 
 
-@router.delete(path="/{role_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def role_delete(role_id: int, repo: RoleRepository = Depends()):
+@router.delete(
+    path="/{role_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Удаление роли",
+    description="Удаляет роль по ее идентификатору (id).",
+)
+async def role_delete(role_id: int, repo: RoleRepository = Depends()) -> None:
     if not await repo.remove(id=role_id):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
