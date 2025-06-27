5f9b2f2763e3ba95d2e02141dcd288ce40942625
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-06-27 11:53:14 +0500

    Add annotation to defect_type

--- backend/app/schemas/defect_type.py
+++ backend/app/schemas/defect_type.py
@@ -1,32 +1,59 @@
+from typing import Annotated
+
 from pydantic import BaseModel, ConfigDict, Field
 
 from app.schemas.photo import PhotoRead
 
+DEFECT_TYPE_FIELDS_CONFIG = {
+    "id": Field(description="Уникальный идентификатор", examples=[1, 2, 3]),
+    "name": Field(
+        description="Название вида дефекта растения",
+        examples=["Некроз", "Трещина"],
+    ),
+    "description": Field(
+        description="Описание вида дефекта",
+        examples=[
+            "Участок с поврежденной или отсутствующей корой",
+            "Глубокий раскол ствола",
+        ],
+    ),
+    "image_ids": Field(
+        default_factory=list,
+        description="Список идентификаторов изображений видов дефектов",
+        examples=[1, 2],
+    ),
+    "images": Field(description="Список изображений вида дефекта"),
+}
+
 
 class DefectTypeBase(BaseModel):
-    name: str
-    description: str | None = None
+    name: Annotated[str, DEFECT_TYPE_FIELDS_CONFIG["name"]]
+    description: Annotated[
+        str | None, DEFECT_TYPE_FIELDS_CONFIG["description"]
+    ] = None
 
 
 class DefectTypeCreate(DefectTypeBase):
-    image_ids: list[int] = Field(default_factory=list)
+    image_ids: Annotated[list[int], DEFECT_TYPE_FIELDS_CONFIG["image_ids"]]
 
 
 class DefectTypeUpdate(BaseModel):
     name: str | None = None
     description: str | None = None
-    image_ids: list[int] | None = None
+    image_ids: Annotated[
+        list[int] | None, DEFECT_TYPE_FIELDS_CONFIG["image_ids"]
+    ] = None
 
 
 class DefectTypeShortRead(BaseModel):
-    id: int
-    name: str
+    id: Annotated[int, DEFECT_TYPE_FIELDS_CONFIG["id"]]
+    name: Annotated[str, DEFECT_TYPE_FIELDS_CONFIG["name"]]
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class DefectTypeRead(DefectTypeBase):
-    id: int
-    images: list[PhotoRead]
+    id: Annotated[int, DEFECT_TYPE_FIELDS_CONFIG["id"]]
+    images: Annotated[list[PhotoRead], DEFECT_TYPE_FIELDS_CONFIG["images"]]
 
     model_config = ConfigDict(from_attributes=True)
