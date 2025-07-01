7576bcaef5b05e9eec4ff533e77c71f4bda8098f
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-01 13:18:17 +0500

    Update routers

--- backend/app/api/routers.py
+++ backend/app/api/routers.py
@@ -1,16 +1,26 @@
 from fastapi import APIRouter
 
-from app.api.v1 import role_router, user_router
+from app.api.v1 import (
+    defect_type_router,
+    photo_router,
+    role_router,
+    user_router,
+)
 
 main_router = APIRouter(prefix="/api/v1")
 
 main_router.include_router(router=user_router)
 main_router.include_router(router=role_router, prefix="/roles", tags=["Роли"])
+main_router.include_router(
+    router=defect_type_router,
+    prefix="/defect-types",
+    tags=["Виды дефектов (справочник)"],
+)
+main_router.include_router(
+    router=photo_router, prefix="/photos", tags=["Фотографии / Изображения"]
+)
 # main_router.include_router(router=team_router, prefix="/teams", tags=["Команды"])
 # main_router.include_router(router=ticket_router, prefix="/tickets", tags=["Заявки"])
 # main_router.include_router(
 #     router=sector_router, prefix="/sectors", tags=["Учетные участки"]
-# )
-# main_router.include_router(
-#     router=defect_router, prefix="/defects", tags=["Виды дефектов (справочник)"]
 # )
--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,2 +1,4 @@
-from app.api.v1.user import router as user_router
+from app.api.v1.defect_type import router as defect_type_router
 from app.api.v1.role import router as role_router
+from app.api.v1.user import router as user_router
+from app.api.v1.photo import router as photo_router
