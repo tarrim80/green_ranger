b8db3ed8269d5ada296de49ef4c7da61f5b83ced
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-01 13:14:11 +0500

    Add custom exceptions

---
+++ backend/app/core/exceptions.py
@@ -0,0 +1,22 @@
+class AppException(Exception):
+    pass
+
+
+class FileProcessingError(AppException):
+    pass
+
+
+class DefectTypeCreationError(AppException):
+    pass
+
+
+class PhotoCreationError(AppException):
+    pass
+
+
+class PhotoRemovingError(AppException):
+    pass
+
+
+class NotFoundError(AppException):
+    pass
