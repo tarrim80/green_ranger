f30e699c3ce1bb55f05264f12dcafb41dd71b774
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-01 13:15:46 +0500

    Update defect type repository

--- backend/app/repositories/defect_type.py
+++ backend/app/repositories/defect_type.py
@@ -1,12 +1,13 @@
-from typing import Annotated
+from typing import Annotated, Sequence
 
 from fastapi import Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from app.core.db import get_async_session
 from app.models import DefectType
 from app.repositories.base import BaseRepository
-from app.repositories.photo import PhotoRepository
 from app.schemas import DefectTypeCreate, DefectTypeUpdate
 
 
@@ -20,23 +21,26 @@ class DefectTypeRepository(
         session: Annotated[
             AsyncSession, Depends(dependency=get_async_session)
         ],
-        photo_repo: Annotated[PhotoRepository, Depends()],
     ) -> None:
         super().__init__(session=session)
-        self.photo_repo = photo_repo
 
-    async def create(self, obj_in: DefectTypeCreate) -> DefectType:
-        obj_data = obj_in.model_dump()
-        image_ids = obj_data.pop("image_ids", [])
-        new_defect_type = self.model(**obj_data)
-        if image_ids:
-            images = await self.photo_repo.get_by_ids(ids=image_ids)
-            if len(images) != len(image_ids):
-                # TODO: Бросить ошибку, если не все фото найдены
-                pass
-            new_defect_type.images = images
+    async def get(self, id: int) -> DefectType | None:
+        statement = (
+            select(self.model)
+            .options(selectinload(self.model.images))
+            .where(self.model.id == id)
+        )
+        result = await self.session.execute(statement=statement)
+        return result.scalar_one_or_none()
 
-        self.session.add(instance=new_defect_type)
-        await self.session.commit()
-        await self.session.refresh(instance=new_defect_type)
-        return new_defect_type
+    async def get_multi(
+        self, skip: int = 0, limit: int = 100
+    ) -> Sequence[DefectType]:
+        statement = (
+            select(self.model)
+            .options(selectinload(self.model.images))
+            .offset(offset=skip)
+            .limit(limit=limit)
+        )
+        result = await self.session.execute(statement=statement)
+        return result.scalars().all()
