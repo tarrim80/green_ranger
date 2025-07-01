1803cf474550d95b90bd1e2e7b976d1046c146e6
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-01 13:16:10 +0500

    Add photo update method

--- backend/app/repositories/photo.py
+++ backend/app/repositories/photo.py
@@ -3,13 +3,14 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.validators import validate_photo_links
 from app.core.db import get_async_session
 from app.models import Photo
 from app.repositories.base import BaseRepository
-from app.schemas import PhotoCreate
+from app.schemas import PhotoCreate, PhotoUpdate
 
 
-class PhotoRepository(BaseRepository[Photo, PhotoCreate, PhotoCreate]):
+class PhotoRepository(BaseRepository[Photo, PhotoCreate, PhotoUpdate]):
     model = Photo
 
     def __init__(
@@ -19,3 +20,14 @@ class PhotoRepository(BaseRepository[Photo, PhotoCreate, PhotoCreate]):
         ],
     ) -> None:
         super().__init__(session=session)
+
+    async def update(self, db_obj: Photo, obj_in: PhotoUpdate) -> Photo:
+        obj_data = {
+            c.name: getattr(db_obj, c.name) for c in db_obj.__table__.columns
+        }
+        update_data = obj_in.model_dump(exclude_unset=True)
+        obj_data.update(update_data)
+
+        validate_photo_links(data=obj_data)
+
+        return await super().update(db_obj=db_obj, obj_in=obj_in)
