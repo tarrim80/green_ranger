804c74b9695736c88d0c8213accb410f18cb3548
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-04 18:49:09 +0500

    Add SurveyRepository

--- backend/app/repositories/survey.py
+++ backend/app/repositories/survey.py
@@ -1,8 +1,11 @@
-from typing import Annotated
+from typing import Annotated, Sequence
 
 from fastapi import Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
+from app.core.constants import DEFAULT_LIMIT
 from app.core.db import get_async_session
 from app.models import Survey
 from app.repositories.base import BaseRepository
@@ -19,3 +22,26 @@ class SurveyRepository(BaseRepository[Survey, SurveyCreate, SurveyUpdate]):
         ],
     ) -> None:
         super().__init__(session=session)
+
+    async def get(self, id: int) -> Survey | None:
+        statement = (
+            select(self.model)
+            .options(selectinload(self.model.tree_photos))
+            .options(selectinload(self.model.survey_defects))
+            .where(self.model.id == id)
+        )
+        result = await self.session.execute(statement=statement)
+        return result.scalar_one_or_none()
+
+    async def get_multi(
+        self, skip: int = 0, limit: int = DEFAULT_LIMIT
+    ) -> Sequence[Survey]:
+        statement = (
+            select(self.model)
+            .options(selectinload(self.model.tree_photos))
+            .options(selectinload(self.model.survey_defects))
+            .offset(offset=skip)
+            .limit(limit=limit)
+        )
+        result = await self.session.execute(statement=statement)
+        return result.scalars().all()
