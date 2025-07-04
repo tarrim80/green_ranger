14deb045803e2d2f2445a73cb32ff0954fc3dc79
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-04 18:39:04 +0500

    Add FAILED_CREATE_SURVEY

--- backend/app/core/constants.py
+++ backend/app/core/constants.py
@@ -12,6 +12,7 @@ class ExceptionDetails:
     FAILED_TO_PROCESSING_FILE = "Ошибка при обработке файла"
     FAILED_CREATE_RECORD = "Ошибка при записи в базу данных"
     FAILED_CREATE_DEFECT_TYPE = "Ошибка при создании вида дефекта"
+    FAILED_CREATE_SURVEY = "Ошибка при создании обследования"
     FAILED_CREATE_SURVEY_DEFECT = "Ошибка при создании обнаруженного дефекта"
     FAILED_CREATE_PHOTO = "Ошибка при создании и привязке фото"
     FAILED_REMOVE_RECORD = "Ошибка при удалении из базы данных"
