43a8c00395680155d7d4d49dda5622a8de2a6d86
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-07 11:36:17 +0500

    Add routers for teams & sectors

--- backend/app/api/routers.py
+++ backend/app/api/routers.py
@@ -4,8 +4,10 @@ from app.api.v1 import (
     defect_type_router,
     photo_router,
     role_router,
+    sector_router,
     survey_defect_router,
     survey_router,
+    team_router,
     user_router,
 )
 
@@ -28,8 +30,10 @@ main_router.include_router(
 main_router.include_router(
     router=survey_router, tags=["Обследования (Осмотры)"]
 )
-# main_router.include_router(router=team_router, prefix="/teams", tags=["Команды"])
-# main_router.include_router(router=ticket_router, prefix="/tickets", tags=["Заявки"])
-# main_router.include_router(
-#     router=sector_router, prefix="/sectors", tags=["Учетные участки"]
-# )
+main_router.include_router(
+    router=sector_router, prefix="/sectors", tags=["Учетные участки"]
+)
+main_router.include_router(
+    router=team_router, prefix="/teams", tags=["Команды"]
+)
+# main_router.include_router(router=tree_router, prefix="/trees", tags=["Растения"])
