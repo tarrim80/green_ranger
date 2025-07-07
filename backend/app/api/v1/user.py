486084e1c8d4265e85b63f59a6f5626637307b36
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-07 11:34:45 +0500

    Add user delete endpoint
    not allowed delete users

--- backend/app/api/v1/user.py
+++ backend/app/api/v1/user.py
@@ -1,9 +1,10 @@
 from typing import Sequence
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.constants import ExceptionDetails
 from app.core.db import get_async_session
 from app.core.user import auth_backend, fastapi_users
 from app.models import User
@@ -44,3 +45,16 @@ async def list_users(
     result = await session.execute(select(User))
     users_db = result.scalars().all()
     return [UserShortRead.model_validate(obj=user_db) for user_db in users_db]
+
+
+@router.delete(
+    "/users/{id}",
+    tags=["Пользователи"],
+    deprecated=True,
+    description="Не используйте удаление, деактивируйте пользователей.",
+)
+def delete_user(id: int):
+    raise HTTPException(
+        status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+        detail=ExceptionDetails.NOT_ALLOWED_REMOVE_USERS,
+    )
