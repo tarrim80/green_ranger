e9e2dfe895f01a124070767ce2b058f562583c3e
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-07-31 14:56:41 +0500

    Add main map

--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Карта</h1>
+  <div style="height: 85vh; width: 100%">
+    <l-map
+      ref="map"
+      v-model:zoom="zoom"
+      :center="[43.238949, 76.889709]"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+        attribution="© <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+      >
+      </l-tile-layer>
+    </l-map>
+  </div>
 </template>
+
+<script setup>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { ref } from "vue";
+
+const zoom = ref(12);
+</script>
