c0d374c9e8b8126566a41f29dd37d1ae03587809
Roman Tarasenko WSL <tarrim80@yandex.ru>
2025-08-03 14:09:14 +0500

    Add getter refresh token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -22,4 +22,46 @@ apiClient.interceptors.request.use(
   }
 );
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const authStore = useAuthStore();
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      if (originalRequest.url.includes("/auth/jwt/refresh")) {
+        authStore.logout();
+        return Promise.reject(error);
+      }
+
+      try {
+        const response = await axios.post(
+          `${apiClient.defaults.baseURL}/auth/jwt/refresh`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${authStore.refreshToken}`,
+            },
+          }
+        );
+
+        await authStore.setTokensAndUser(response.data);
+
+        originalRequest.headers[
+          "Authorization"
+        ] = `Bearer ${authStore.accessToken}`;
+
+        return apiClient(originalRequest);
+      } catch (refreshError) {
+        authStore.logout();
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
